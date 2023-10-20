236d4f89fc1c08e6d247cc9668f79f2b2a5cc947
marianoariassoy <hola@marianoarias.soy>
2023-10-19 23:08:17 -0300

    add dao and classes

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose'
 export const mongoConnect = () => {
   mongoose
     .connect(process.env.DB_URL)
-
     .then(() => {
       console.log('Connected to the database')
     })
